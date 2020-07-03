fe777893620b92a4d117794af912d14d2ba4e949
KylinDC <inlovingdays@gmail.com>
2020-07-03 23:44:02 +0800

    finish all practice

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,15 @@
-export const getPoetry
+export const getPoetry = () =>
+  fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => [data.origin, data.author, data.content])
+    .catch((err) => console.log(err));
+
+export const getPoetry2 = async () => {
+  try {
+    const response = await fetch("https://v1.jinrishici.com/all.json");
+    const data = await response.json();
+    return [data.origin, data.author, data.content];
+  } catch (err) {
+    console.log(err);
+  }
+};
